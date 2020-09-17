1f892eb01f222bb99551899c73c9e63bf5a02f14
Costel Dragu <costel.dragu@gmail.com>
2020-09-17 17:52:14 +0300

    First controllers

---
+++ src/main/java/mdc/tacocloud/controller/HomeController.java
@@ -0,0 +1,18 @@
+package mdc.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}
